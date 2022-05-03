a0dbba9b01722139d18cc3c53aa116aa819a4321
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-05-03 14:03:09 -0700

    commit before BCS help

--- script.js
+++ script.js
@@ -3,9 +3,7 @@
 
  function init() {
      changeColor()
-    // checkTime every 5 minutes
-     
-
+    
  }
 
 // Current date displayed at top of page
@@ -24,22 +22,20 @@ function changeColor() {
     console.log(currentTime)
 }
 
-    // // will need this in that function
-//     let taskArea = $(".description");   
 //     //remove any old classes from element
-//     $(taskArea).removeClass(".present .past .future");
+    $("description").removeClass(".present .past .future");
 
 //     // apply new class based on if the description row is past, present, or future
-//     if (currentTime > hour) {
-//         $(this).addClass('past');
-//     };
-//     if (currentHour === hour) {
-//         $(this).addClass('present');
-//     };
-//     if (currentHour < hour) {
-//         $(this).addClass('future');
-//     }
-// };
+    if (currentTime > hour) {
+        $("description").addClass('past');
+    };
+    if (currentHour === hour) {
+        $("description").addClass('present');
+    };
+    if (currentHour < hour) {
+        $("description").addClass('future');
+    }
+
 
 
 // // when a save button is pressed it saves task to local storage
