a6779da3cd3ba5346f127a5a34d55c4a6d27ac86
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-05-03 12:06:29 -0700

    updated ids for boxes

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // Date displayed at top of page
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+var timeBlockElements = $(".col-md-10 description");
+
+// get current time
+var currentTime = moment().format('H');
+
+// function to use current time to dictate color of boxes 
