03413e2fbbaddd959191c7e05686ee8c60487e58
Roya-Jamshidpour <roya.jamshidpour@gmail.com>
2022-05-04 15:11:37 -0700

    formatted document

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ init()
 
 function init() {
     changeColor()
-    
+
 }
 
 
@@ -19,14 +19,14 @@ $('.saveBtn').on('click', saveTask);
 
 // saves task to local storage
 function saveTask() {
-// get the text typed inside row
-let time = $(this).parent().attr('data-time');
-let text = $(this).prev().val();
+    // get the text typed inside row
+    let time = $(this).parent().attr('data-time');
+    let text = $(this).prev().val();
 
-// set item in local storage
-localStorage.setItem(time, text);
-console.log(text);
-displayTask(time, text)
+    // set item in local storage
+    localStorage.setItem(time, text);
+    console.log(text);
+    displayTask(time, text)
 
 }
 
@@ -45,11 +45,11 @@ function changeColor() {
         // applies color classes based on hour of day
         if (currentTime > hour) {
             $(this).addClass('past');
-        
+
         } else if (currentTime === hour) {
             $(this).addClass('present');
-    
-    } else {
+
+        } else {
             $(this).addClass('future');
         }
     });
